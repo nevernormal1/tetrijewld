f369242dfe993293d74de8c021ba67d06d22a376
Jeff Talbot <jeff.a.talbot@gmail.com>
2022-01-01 10:28:58 -0600

    Implemented rotation for piece5

--- src/features/game/pieces/piece5.js
+++ src/features/game/pieces/piece5.js
@@ -2,6 +2,33 @@
 //    *
 //    *
 const Piece5 = ({ piece }) => {
+  if (piece.rotation === 270) {
+    return [
+      [0, 1],
+      [0, 0],
+      [1, 1],
+      [2, 1],
+    ];
+  }
+
+  if (piece.rotation === 180) {
+    return [
+      [0, 2],
+      [1, 2],
+      [1, 1],
+      [1, 0],
+    ];
+  }
+
+  if (piece.rotation === 90) {
+    return [
+      [2, 1],
+      [2, 0],
+      [1, 0],
+      [0, 0],
+    ];
+  }
+
   return [
     [0, 0],
     [1, 0],
