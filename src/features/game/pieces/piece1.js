9d4fd0078e8cf5b5b52e23c549674e3c833527e0
Jeff Talbot <jeff.a.talbot@gmail.com>
2022-01-15 11:10:15 -0600

    Add more rotation positions since color matters

--- src/features/game/pieces/piece1.js
+++ src/features/game/pieces/piece1.js
@@ -2,21 +2,35 @@
 //  **
 const Piece1 = ({ piece }) => ({
   offsets: () => {
-    if (piece.rotation === 270 || piece.rotation === 90) {
+    if (piece.rotation === 0) {
+      return [
+        [1, 0],
+        [2, 0],
+        [0, 1],
+        [1, 1],
+      ];
+    } else if (piece.rotation === 270) {
       return [
         [0, 1],
         [0, 0],
         [1, 2],
         [1, 1],
       ];
+    } else if (piece.rotation === 180) {
+      return [
+        [1, 1],
+        [0, 1],
+        [2, 0],
+        [1, 0],
+      ];
+    } else if (piece.rotation === 90) {
+      return [
+        [1, 1],
+        [1, 2],
+        [0, 0],
+        [0, 1],
+      ];
     }
-
-    return [
-      [1, 0],
-      [2, 0],
-      [0, 1],
-      [1, 1],
-    ];
   },
 
   width: () => {
--- src/features/game/pieces/piece2.js
+++ src/features/game/pieces/piece2.js
@@ -2,21 +2,35 @@
 //    **
 const Piece2 = ({ piece }) => ({
   offsets: () => {
-    if (piece.rotation === 270 || piece.rotation === 90) {
+    if (piece.rotation === 0) {
       return [
-        [0, 2],
+        [0, 0],
+        [1, 0],
+        [1, 1],
+        [2, 1],
+      ];
+    } else if (piece.rotation === 90) {
+      return [
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+      ];
+    } else if (piece.rotation === 180) {
+      return [
+        [1, 0],
+        [0, 1],
+        [1, 1],
+        [1, 0],
+      ];
+    } else if (piece.rotation === 270) {
+      return [
+        [1, 0],
         [0, 1],
         [1, 1],
         [1, 0],
       ];
     }
-
-    return [
-      [0, 0],
-      [1, 0],
-      [1, 1],
-      [2, 1],
-    ];
   },
   width: () => {
     return piece.rotation === 270 || piece.rotation === 90 ? 2 : 3;
